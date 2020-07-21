538214851e9a580deedadee04a17c56ee5088b94
Archil Chovatiya <chovatiya.archil@gmail.com>
2020-07-22 02:47:47 +0530

    Updates

---
+++ precache-manifest.22a2e0945f0a14c3ab94d9142ae29b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52bde118530b5889831da1fc0c53df41",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c9bf25445199aaeff695",
+    "url": "/monster-rolodex/static/css/main.31b3547e.chunk.css"
+  },
+  {
+    "revision": "8034fbb033b211697792",
+    "url": "/monster-rolodex/static/js/2.dc87de0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.dc87de0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9bf25445199aaeff695",
+    "url": "/monster-rolodex/static/js/main.72af5979.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
